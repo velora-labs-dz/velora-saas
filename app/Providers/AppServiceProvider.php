fb0c6eee2fa68c26513adcd61335d80e095b58e2
rouabah zin eddine <peeplil6666@gmail.com>
2026-08-26 04:37:58 +0200

    app service provider https url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,9 @@
 namespace App\Providers;
 
 use App\Support\CurrentOrganization;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -30,5 +30,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
